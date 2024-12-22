8be6c3a84c6761313cc6fef959db911b7c82850e
LucasMalagoli <mpslucas14@gmail.com>
2024-12-22 09:51:13 -0300

    remove prints and reuse screen

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color=(0, 0, 0))
+        screen.fill(color=(0, 0, 0))
         pygame.display.flip()
 
 
